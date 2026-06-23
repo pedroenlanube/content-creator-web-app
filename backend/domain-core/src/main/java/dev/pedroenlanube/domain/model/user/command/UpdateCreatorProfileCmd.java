6909f64931276b095f68742f72a19429830ad2fb
pedroenlanube <github@pedroenlanube.dev>
2026-06-23 21:13:40 +0200

    feat: add definition of common entity model

--- backend/domain-core/src/main/java/dev/pedroenlanube/domain/model/user/command/UpdateCreatorProfileCmd.java
+++ backend/domain-core/src/main/java/dev/pedroenlanube/domain/model/user/command/UpdateCreatorProfileCmd.java
@@ -1,11 +1,19 @@
 package dev.pedroenlanube.domain.model.user.command;
 
+import dev.pedroenlanube.domain.model.user.vo.SocialMediaLink;
+
+import java.util.List;
+
 public record UpdateCreatorProfileCmd(
         String creatorId,
         String username,
         String biography,
         String avatarUrl,
         String bannerUrl,
-        String socialMedia //TODO revisar esto, cambiar por un listado de redes con tipo (enum) y url?
+        List<SocialMediaLink> socialMediaLinks
 ) {
+    public UpdateCreatorProfileCmd {
+        if(socialMediaLinks == null)
+            socialMediaLinks = List.of();
+    }
 }
