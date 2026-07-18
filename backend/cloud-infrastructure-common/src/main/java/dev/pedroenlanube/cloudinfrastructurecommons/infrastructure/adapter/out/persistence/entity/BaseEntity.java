4daa95897fd355ee318a80c1eff79493dc9bf016
pedroenlanube <github@pedroenlanube.dev>
2026-07-18 18:39:24 +0200

    feat: add changes to BaseEntity

--- backend/cloud-infrastructure-common/src/main/java/dev/pedroenlanube/cloudinfrastructurecommons/infrastructure/adapter/out/persistence/entity/BaseEntity.java
+++ backend/cloud-infrastructure-common/src/main/java/dev/pedroenlanube/cloudinfrastructurecommons/infrastructure/adapter/out/persistence/entity/BaseEntity.java
@@ -5,7 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import software.amazon.awssdk.enhanced.dynamodb.extensions.annotations.DynamoDbVersionAttribute;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*;
+
+import java.time.Instant;
 
 @Setter
 @EqualsAndHashCode
@@ -18,6 +21,15 @@ public class BaseEntity {
     private String sk;
     private String gsi1pk;
     private String gsi1sk;
+    private String gsi2pk;
+    private String gsi2sk;
+
+    // -- Core Attributes
+    private EntityType entityType;
+    private Long version;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
 
     @DynamoDbPartitionKey
     @DynamoDbAttribute("pk")
@@ -34,4 +46,28 @@ public class BaseEntity {
     @DynamoDbSecondarySortKey(indexNames = {"GSI1"})
     @DynamoDbAttribute("gsi1sk")
     public String getGsi1sk() { return gsi1sk; }
+
+    @DynamoDbSecondaryPartitionKey(indexNames = {"GSI2"})
+    @DynamoDbAttribute("gsi2pk")
+    public String getGsi2pk() { return gsi2pk; }
+
+    @DynamoDbSecondarySortKey(indexNames = {"GSI2"})
+    @DynamoDbAttribute("gsi2sk")
+    public String getGsi2sk() { return gsi2sk; }
+
+    @DynamoDbAttribute("entityType")
+    public EntityType getEntityType() { return entityType; }
+
+    @DynamoDbVersionAttribute
+    @DynamoDbAttribute("version")
+    public Long getVersion() { return version; }
+
+    @DynamoDbAttribute("createdAt")
+    public Instant getCreatedAt() { return createdAt; }
+
+    @DynamoDbAttribute("updatedAt")
+    public Instant getUpdatedAt() { return updatedAt; }
+
+    @DynamoDbAttribute("deletedAt")
+    public Instant getDeletedAt() { return deletedAt; }
 }
---
+++ backend/cloud-infrastructure-common/src/main/java/dev/pedroenlanube/cloudinfrastructurecommons/infrastructure/adapter/out/persistence/entity/EntityType.java
@@ -0,0 +1,9 @@
+package dev.pedroenlanube.cloudinfrastructurecommons.infrastructure.adapter.out.persistence.entity;
+
+public enum EntityType {
+    USER,
+    CONTENT_DOCUMENT,
+    CONTENT_IMAGE,
+    CONTENT_VIDEO,
+    COACHING_SESSION
+}
